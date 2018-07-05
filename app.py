7c57b71e937a1a66a557c002ec75b1e8cf7dd244
Jesus Urrutia <jesus.urrutia@gmail.com>
2018-07-05 18:37:29 -0400

    Added private area

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+import os
+from functools import wraps
+from flask import Flask, render_template, session, url_for, request, redirect
 
 app = Flask(__name__)
+
+app.secret_key = os.getenv('SECRET_KEY')
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'username' not in session:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
 
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+@app.route('/private')
+@login_required
+def private():
+    return render_template('private.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = 'username'
+        return redirect(url_for('private'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect(url_for('login'))
 
 @app.errorhandler(404)
 def page_not_found(error):
